d7ba49bc4c3d34cd18283f6818b12995a0e95bbd
Daniel-laptop <d.lambord324@gmail.com>
2022-10-02 00:56:11 +0300

    I added a function that let the ports transfer data until the the char 0 received then it print "end massage" and stop transforming data bugs I need to fix: 1. writing a port that doesn't exists 2. noticing the original port

--- try.py
+++ try.py
@@ -1,6 +1,6 @@
 import serial
 
-
+# noticing the source port and returning the data from it 
 def sendToApp(sorcePort = ""):
 	destinationPort = serial.Serial(port=sorcePort)
 	while(True):
@@ -13,33 +13,29 @@ def sendToApp(sorcePort = ""):
 			if(decodedData != None):
 				return decodedData
 
-
+# noticing the distination port and sending the data to him
 def getFromApp(data, destinationPort =""):
 	print(type(data))
+	print(destinationPort)
 	destinationPort = serial.Serial(port = destinationPort)
 	destinationPort.write(data.encode('utf-8'))
 
+
+# main
 if __name__ == "__main__":
-do{
-	sorcePort = input('who you send to')
-	destinationPort = input('from who you send')
-}
-while(realData!='0'):
-		if(realData=='1'):
-			
+# getting information from the user
+	sorcePort = input('who you send to: ')
+	destinationPort = input('from who you send: ')
+# letting the loop start
+	realData = '1'
+	'''
+		a loop that transforming data from port to port and stop
+		tranforming when the data equals '0' than in the else term
+		sending the last massage that says "end massage" 
+	'''
+	while(realData!='0'):
 		realData = sendToApp(sorcePort = sorcePort)
 		if (realData != '0'):
 			getFromApp(realData,destinationPort = destinationPort)
-
-
-	# printMessage(msg="junk", sendTo=10)
-	# print(type("String"))
-	# myPort = serial.Serial()
-	# print(type(myPort))
-	# myPort.open()
-	# serial.Serial().write()
-	# ksdfsdf
-
-
-
-	
+		else:
+			getFromApp("end massage",destinationPort = destinationPort)
