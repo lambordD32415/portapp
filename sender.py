0f316674fa4c5ce88b12564b15c057d1598586f6
Daniel-laptop <d.lambord324@gmail.com>
2022-10-05 19:56:51 +0300

    in this version i fixed the bugs from the last one and i add a function that gets a port connecting to it cleans it from data before using it and catching exceptions along with that i made the code more readable and comfortable to understand
    in the next ill start working on gui for the app and make an option for more ports to connect and send to each other

--- sender.py
+++ sender.py
@@ -1,10 +1,15 @@
 import serial
 import serial.tools.list_ports
 
-for port in serial.tools.list_ports.comports():
-	print(port.hwid)
+# for port in serial.tools.list_ports.comports():
+	# print(port.name)
 	
+testPort = serial.Serial()
+testPort.port = "\\\\.\\COM4"
 	
+print(testPort.is_open)
+
+
+testPort.open()
+print(testPort.is_open)
 	
-	# 65651261
-	# asdasfadf
--- try.py
+++ try.py
@@ -1,41 +1,57 @@
 import serial
 
-# noticing the source port and returning the data from it 
-def sendToApp(sorcePort = ""):
-	destinationPort = serial.Serial(port=sorcePort)
+# gets the waiting data in the port and returns it 
+def getWaitingDataFromPort(port = serial.Serial.port):
 	while(True):
-		waitingToRead = destinationPort.in_waiting
-		if(waitingToRead != 0):
-			print(waitingToRead)
-			data = destinationPort.read(waitingToRead)
-			decodedData = data.decode('utf-8')
-			print(decodedData)
-			if(decodedData != None):
-				return decodedData
+		data = port.read(port.in_waiting)
+		decodedData = data.decode('utf-8')
+		return decodedData
 
-# noticing the distination port and sending the data to him
-def getFromApp(data, destinationPort =""):
-	print(type(data))
-	print(destinationPort)
-	destinationPort = serial.Serial(port = destinationPort)
+# sending data to a port
+def sendDataToPort(data, destinationPort = serial.Serial.port):
+	print(destinationPort) 
 	destinationPort.write(data.encode('utf-8'))
 
+# openning the port that going to use and checking if it proper
+def getPortNameAndConnect(portToEnter):
+# letting the loop start
+	numErr = 1
+	'''
+	a loop that asks the user which port to connect and connect it
+	but if the name of the port is not exist or already open then he asks agian 
+	as well numErr shows if thier is any errors in a way that
+	if numErr is equal 1 thier is an error and if it equals 0 so thier are no errors at all
+	'''
+	while(numErr != 0):
+		try:
+			portName = "\\\\.\\" + input("Enter " + portToEnter + " Port To Connect ")
+			port = serial.Serial(portName)
+			numErr = 0
+			# clears the waiting data before use it
+			port.read(port.in_waiting)
+			return port
+		except serial.serialutil.SerialException:
+			print("ERROR - this port not found or already open")
 
 # main
 if __name__ == "__main__":
-# getting information from the user
-	sorcePort = input('who you send to: ')
-	destinationPort = input('from who you send: ')
+# getting information from the user and openning the useing ports
+	originalPort = getPortNameAndConnect("Original")
+	virtualPort = getPortNameAndConnect("Virtual")
 # letting the loop start
-	realData = '1'
+	data = '1'
 	'''
-		a loop that transforming data from port to port and stop
-		tranforming when the data equals '0' than in the else term
-		sending the last massage that says "end massage" 
+		a loop that getting waiting data from ports
+		and transforming it from original port to a virtual ports and vice versa 
+		than it stops tranforming when the data equals '0' and in the last if term
+		thier is a command that prints "the transforming stoped" in the console 
 	'''
-	while(realData!='0'):
-		realData = sendToApp(sorcePort = sorcePort)
-		if (realData != '0'):
-			getFromApp(realData,destinationPort = destinationPort)
-		else:
-			getFromApp("end massage",destinationPort = destinationPort)
+	while(data != '0'):
+		if(originalPort.in_waiting != 0):
+			data = getWaitingDataFromPort(originalPort)
+			sendDataToPort(data, virtualPort)
+		elif(virtualPort.in_waiting != 0):
+			data = getWaitingDataFromPort(virtualPort)
+			sendDataToPort(data, originalPort)
+		if(data == '0'):
+			print("the transforming stoped")
