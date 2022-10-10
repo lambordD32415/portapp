66f21599950aa9cd3b192d7859ceb842a925e6e3
Daniel-laptop <d.lambord324@gmail.com>
2022-10-10 16:32:25 +0300

    this version i corrected the code reviews and i moved most of the main commands to a function named runTheTransmission to be able to use it in imports and next steps of the application
    next time ill start the GUI for the application

--- try.py
+++ try.py
@@ -1,43 +1,41 @@
 import serial
+# import threading
 
 # gets the waiting data in the port and returns it 
-def getWaitingDataFromPort(port = serial.Serial.port):
-	while(True):
-		data = port.read(port.in_waiting)
-		decodedData = data.decode('utf-8')
-		return decodedData
+def getWaitingDataFromPort(port):
+	data = port.read(port.in_waiting)
+	decodedData = data.decode('utf-8')
+	return decodedData
 
 # sending data to a port
-def sendDataToPort(data, destinationPort = serial.Serial.port):
-	print(destinationPort) 
+def sendDataToPort(data, destinationPort):
 	destinationPort.write(data.encode('utf-8'))
 
 # openning the port that going to use and checking if it proper
 def getPortNameAndConnect(portToEnter):
 # letting the loop start
-	numErr = 1
+	port = serial.Serial()
 	'''
-	a loop that asks the user which port to connect and connect it
-	but if the name of the port is not exist or already open then he asks agian 
-	as well numErr shows if thier is any errors in a way that
-	if numErr is equal 1 thier is an error and if it equals 0 so thier are no errors at all
+	a loop that asks the user which port to connect and connecting it
+	but if the name of the port is not exists or already opened then he asks again 
+	as well their is a serial.Serial variable named port without any serial.Serial.port
+	information so when it comes to the while loop its checking if its not open 
+	and for the first time the condition equals true and then he  opens the port just 
+	if the port is currect and then it gets out from the loop
 	'''
-	while(numErr != 0):
+	while(not port.is_open):
 		try:
-			portName = "\\\\.\\" + input("Enter " + portToEnter + " Port To Connect ")
-			port = serial.Serial(portName)
-			numErr = 0
-			# clears the waiting data before use it
-			port.read(port.in_waiting)
-			return port
+			port.port = "\\\\.\\" + input("Enter " + portToEnter + " Port To Connect ")
+			port.open()
 		except serial.serialutil.SerialException:
-			print("ERROR - this port not found or already open")
+			print("ERROR - this port not found or already opened")
+			
+	# clears the waiting data before use it
+	port.read(port.in_waiting)
+	return port
 
-# main
-if __name__ == "__main__":
-# getting information from the user and openning the useing ports
-	originalPort = getPortNameAndConnect("Original")
-	virtualPort = getPortNameAndConnect("Virtual")
+# this function runs all the transmission between the original port and a virtual port and stopping it.
+def runTheTransmission(originalPort, virtualPort):
 # letting the loop start
 	data = '1'
 	'''
@@ -55,3 +53,20 @@ if __name__ == "__main__":
 			sendDataToPort(data, originalPort)
 		if(data == '0'):
 			print("the transforming stoped")
+
+
+# main
+if __name__ == "__main__":
+# getting information from the user and openning the useing ports
+	originalPort = getPortNameAndConnect("Original")
+	virtualPort_A = getPortNameAndConnect("Virtual a")
+	# virtualPort_B = getPortNameAndConnect("Virtual b")
+	
+	# firstSplit = threading.Thread(target=runTheTransmission, args = [originalPort, virtualPort_A])
+	# secondSplit = threading.Thread(target=runTheTransmission, args = [originalPort, virtualPort_B])
+	
+	# firstSplit.start()
+	# secondSplit.start()
+	
+	# runTheTransmission(originalPort,virtualPort_A)
+	runTheTransmission(originalPort,virtualPort_B)
